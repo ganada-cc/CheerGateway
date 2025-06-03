c098e84de07c4e56c1101c8f0df72bb96da71389
elive7 <parksub020704@gmail.com>
2025-06-03 15:41:01 +0900

    fix: cookieParser 제거

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cookieParser());
