604231e7790c3ebfa85f780ffdaa5fa951454fa8
ssohy <sohyeonj415@gmail.com>
2025-06-05 11:16:24 +0000

    fix: 경로 수정3

--- main.js
+++ main.js
@@ -74,7 +74,6 @@ const cssCommunityProxy = createProxyMiddleware({
 const mindDiaryProxy = createProxyMiddleware({
   target: 'http://minddiary.default.svc.cluster.local',
   changeOrigin: true,
-  pathRewrite: { '^/minddiary': '/minddiary' }, // 수정
   onProxyReq: (proxyReq, req) => {
     console.log('[PROXY] onProxyReq 호출됨');
     console.log('[PROXY] 요청 URL:', req.originalUrl);
